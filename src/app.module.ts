afd596f476a58a69c24f913917962fef08e0eaf7
Amox111 <ruxkwarmmana233@gmail.com>
2026-03-25 12:06:13 +0700

    g

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,11 @@ import { APP_FILTER } from '@nestjs/core';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ApiExceptionFilter } from './common/api-exception.filter';
+import { PurchaseModule } from './purchases/purchase.module';
+import { ProductModule } from './products/product.module';
 
 @Module({
-  imports: [],
+  imports: [PurchaseModule, ProductModule],
   controllers: [AppController],
   providers: [
     AppService,
